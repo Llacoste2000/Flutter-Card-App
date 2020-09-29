61288c948d45296f2d46de984f50b88629428294
llacoste2000 <louislacoste33@gmail.com>
2020-09-29 17:00:24 +0200

    update(Main): Starting main project, added Profile image.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 String primaryColor = '#FFB600'.replaceAll('#', '0xff');
+AssetImage pdp = new AssetImage('assets/images/Louis.jpg');
 
 void main() {
   runApp(MyApp());
@@ -12,9 +14,22 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Color(int.parse(primaryColor)),
         body: SafeArea(
-          child: ,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    backgroundImage: pdp,
+                    radius: 50.0,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
